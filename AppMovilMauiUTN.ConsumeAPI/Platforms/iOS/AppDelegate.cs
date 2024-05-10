ee8af01969fbf1f4c5c5c9e30231a804b1651420
bryanf1925 <bdfuentesm@utn.edu.ec>
2024-05-09 23:45:52 -0500

    Primer commit MAUI

---
+++ AppMovilMauiUTN.ConsumeAPI/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace AppMovilMauiUTN.ConsumeAPI
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
